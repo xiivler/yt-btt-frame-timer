2d0618096c1c8734dc831a909754836c7c543b9c
xiivler <51525828+xiivler@users.noreply.github.com>
2021-09-22 16:47:29 -0700

    Update main.js

--- main.js
+++ main.js
@@ -1,85 +1,28 @@
 function compute() {
 
-    // Initiate basic time variables
-    let hours = 0;
-    let minutes = 0;
-    let seconds = 0;
-    let milliseconds = 0;
-
-    // Get framerate, start frame, and end frame from corresponding elements
+    // Get framerate, start time, and end time from corresponding elements
     // Double check they all have a value
-    let frameRate = parseInt(document.getElementById('framerate').value);
-    let startFrame = document.getElementById('startobj').value;
-    let endFrame = document.getElementById('endobj').value;
-    if (typeof (startFrame) === 'undefined' || endFrame === 'undefined' || framerate === 'undefined') {
+    let frameRate = 60;
+    let startTime = document.getElementById('startobj').value;
+    let endTime = document.getElementById('endobj').value;
+    if (typeof (startTime) === 'undefined' || endTime === 'undefined' || frameRate === 'undefined') {
         return
-    };
-
-    // Calculate framerate
-    let frames = (endFrame - startFrame) * frameRate;
-    seconds = Math.floor(frames / frameRate);
-    frames = frames % frameRate;
-    milliseconds = Math.round(frames / frameRate * 1000);
-    if (milliseconds < 10) {
-        milliseconds = '00' + milliseconds;
-    } else if (milliseconds < 100) {
-        milliseconds = '0' + milliseconds;
     }
-    if (seconds >= 60) {
-        minutes = Math.floor(seconds / 60);
-        seconds = seconds % 60;
-        seconds = seconds < 10 ? '0' + seconds : seconds;
-    }
-    if (minutes >= 60) {
-        hours = Math.floor(minutes / 60);
-        minutes = minutes % 60;
-        minutes = minutes < 10 ? '0' + minutes : minutes;
-    }
+    
+    //calculate time in seconds and frames
+    let time = endTime - startTime;
+    let seconds = Math.floor(time);
+    let frames = Math.round((time - seconds) * 60);
 
     // Show the time and mod message in the DOM
-    let finalTime = hours.toString() + 'h ' + minutes.toString() + 'm ' + seconds.toString() + 's ' + milliseconds.toString() + 'ms';
-    let modMessage = `Mod Message: Time starts at ${parseFloat(startFrame).toFixed(3)} and ends at ${parseFloat(endFrame).toFixed(3)} at ${frameRate} fps to get a final time of ${finalTime}.`;
-    let credits = `Retimed using [yt-frame-timer](https://slashinfty.github.io/yt-frame-timer)`;
+    let finalTime = seconds + ':' + String(frames).padStart(2, '0');
     document.getElementById('time').value = finalTime;
-    document.getElementById('modMessage').disabled = false;
-    document.getElementById('modMessage').innerText = modMessage + ' ' + credits;    
-    document.getElementById("modMessageButton").disabled = false;
-}
-
-function copyModMessage() {
-	// Allow user to copy mod message to clipboard
-	const textArea = document.getElementById('modMessage');
-	textArea.focus();
-	textArea.select();
-	document.execCommand('copy');
-	alert(`The mod message has been copied to clipboard! Please paste it into the comment of the run you are verifying.`);
-}
-
-const validateFPS = (event) => {
-    // If framerate is invalid, show an error message and disable start and end frame fields
-    if (event.target.value === '' || parseInt(event.target.value) <= 0 || isNaN(parseInt(event.target.value))) {
-        document.getElementById('framerate').setCustomValidity('Please enter a valid framerate.');
-        document.getElementById('framerate').reportValidity();
-        document.getElementById('startobj').disabled = true;
-        document.getElementById('endobj').disabled = true;
-        document.getElementById('computeButton').disabled = true;
-    } else {
-        document.getElementById('startobj').disabled = false;
-        document.getElementById('endobj').disabled = false;
-        document.getElementById('computeButton').disabled = false;
-    }
 }
 
 const parseForTime = (event) => {
     // Get current frame from input field (either start time or end time)
     let frameFromInputText = (JSON.parse(event.target.value)).lct;
     if (typeof frameFromInputText !== 'undefined') {
-        // Get the framerate
-        let frameRate = parseInt(document.getElementById('framerate').value);
-        // Calculate the frame
-        let frameFromObj = (time, fps) => Math.ceil(time * fps) / fps; //round to the nearest frame
-        let finalFrame = frameFromObj(frameFromInputText, frameRate);
-        // Update the DOM
-        document.getElementById(event.target.id).value = `${finalFrame}`;
+        document.getElementById(event.target.id).value = frameFromInputText;
     }
 }
